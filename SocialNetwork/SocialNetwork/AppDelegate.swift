44d7beced58a693f13b0a1dcacfd5823e32820c2
Kiriienko Taras <kiriyenko.work@gmail.com>
2019-11-28 20:06:02 +0100

    project init

---
+++ SocialNetwork/SocialNetwork/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  SocialNetwork
+//
+//  Created by Taras Kiriienko on 28/11/2019.
+//  Copyright © 2019 Taras Kiriienko. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ SocialNetwork/SocialNetwork/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  SocialNetwork
+//
+//  Created by Taras Kiriienko on 28/11/2019.
+//  Copyright © 2019 Taras Kiriienko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+}
+
